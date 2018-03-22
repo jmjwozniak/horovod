0f29eb4d66b7902319584a901994aadc31dd6e53
Justin Wozniak <wozniak@mcs.anl.gov>
2018-03-22 11:14:42 -0500

    Support a test ID for multiple tests

--- controller/tests/test-keras_mnist_short.sh
+++ controller/tests/test-keras_mnist_short.sh
@@ -8,13 +8,21 @@ set -eu
 MPIEXEC=mpiexec.mpich
 # USER SETTINGS END
 
+if [ ${#} != 1 ]
+then
+  echo "usage: test-keras_mnist_short.sh <ID>"
+  echo "       where ID is the test ID"
+  exit 1
+fi
+ID=$1
+
 THIS=$( cd $( dirname $0 ) ; /bin/pwd )
 cd $THIS/..
 make tests/test-keras_mnist_short.x
 cd $THIS
 
 HOROVOD=$( cd $THIS/../.. ; /bin/pwd )
-EXAMPLE_MNIST=$HOROVOD/examples/keras_mnist_short.py
+EXAMPLE_MNIST=$HOROVOD/examples/keras_mnist_short$ID.py
 
 if ! $MPIEXEC -n 2 ./test-keras_mnist_short.x $EXAMPLE_MNIST
 then
